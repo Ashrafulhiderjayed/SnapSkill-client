729b0553f3860ee1a1eadc05bbe77683a69bcb58
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-06-16 22:19:27 +0600

    instructors page

---
+++ src/Pages/InstructorInfo/InstructorInfo.jsx
@@ -0,0 +1,16 @@
+
+const InstructorInfo = ({ instructor }) => {
+  const { photoURL, name } = instructor;
+  return (
+    <div className='card  bg-base-100 shadow-xl'>
+      <figure>
+        <img src={photoURL} alt='Shoes' />
+      </figure>
+      <div className='card-body'>
+        <h2>Instructor Name: {name}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default InstructorInfo;
